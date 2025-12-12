4224729ba2ea7b164f724866ff57069adea6415f
adil-mabrouk <mabroukeadil@gmail.com>
2025-12-12 16:36:36 +0100

    main

--- Server/main.cpp
+++ Server/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <csignal>
 #include <cstdlib>
+#include "../cgi/cgi.hpp"
 
 volatile sig_atomic_t g_running = 1;
 
@@ -14,37 +15,12 @@ void signalHandler(int signum)
 
 int main()
 {
-	std::cout << "╔══════════════════════════════════╗" << std::endl;
-	std::cout << "║     Webserv HTTP Server          ║" << std::endl;
-	std::cout << "║     C++98 Implementation         ║" << std::endl;
-	std::cout << "╚══════════════════════════════════╝" << std::endl;
-	std::cout << std::endl;
+	CGI cgi;
+    cgi.setScriptPath("/home/amabrouk/Desktop/webserv/test.py");
+    cgi.setMethod("GET");
+    cgi.setQueryString("name=Alice");
 
-	try
-	{
-		Server server;
-
-		// Run server
-		server.run();
-		
-		std::cout << "\n" << std::string(40, '=') << std::endl;
-		std::cout << "[STATUS] Server shutdown complete" << std::endl;
-	}
-	catch (const std::runtime_error& e)
-	{
-		std::cerr << "\n[FATAL] Runtime error: " << e.what() << std::endl;
-		return 1;
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "\n[FATAL] Exception: " << e.what() << std::endl;
-		return 1;
-	}
-	catch (...)
-	{
-		std::cerr << "\n[FATAL] Unknown error occurred" << std::endl;
-		return 1;
-	}
-	
-	return 0;
+    
+    std::string response = cgi.execute();
+	std::cout << response << std::endl;
 }
