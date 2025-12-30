01fe22b240666ce058fb65c1aed6e5589c7c07fa
amabrouk <mabroukadil85@gmail.com>
2025-12-30 05:02:03 +0100

    cgi response

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "Hello from PHP!\n";
-echo "Request method: " . $_SERVER['REQUEST_METHOD'] . "\n";
-echo "Current time: " . date("Y-m-d H:i:s") . "\n";
-?>
---
+++ www/var/timeout.py
@@ -0,0 +1,3 @@
+import os
+# os.system("curl localhost:7070/cgi-bin/timeout.py")
+os.system("sleep 10")
